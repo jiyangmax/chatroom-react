825caa49ec1850b6014c91cb7d15c2a326471288
jiyang <jiyangmax@163.com>
2022-04-21 23:14:58 +0800

    add layout

--- src/chat/Chatbox.js
+++ src/chat/Chatbox.js
@@ -1,18 +1,27 @@
-import React, {Component } from 'react';
-import { Row, Col } from 'antd';
+import React, { Component } from 'react';
+import { Row, Col, Input } from 'antd';
 import './Chatbox.css';
 
+const { TextArea } = Input;
+
 class Chatbox extends Component {
-    render() {
-        return <div className="chatbox">
-        <Row className="chatbox-row">
-          <Col className="chatbox-col" span={12} offset={6}>
-            <div className="chatbox-content"></div>
-          
-          </Col>
-        </Row>
-      </div>
-    }
+  render() {
+    return <div className="chatbox">
+      <Row className="chatbox-row">
+        <Col className="chatbox-col" span={12} offset={6}>
+           <div className="chatbox-header">
+          </div>
+          <div className="chatbox-content">
+          </div>
+          <Row className="chatbox-input">
+            <Col span={18}>
+              <TextArea type="textarea" rows={4} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  }
 
 }
 
